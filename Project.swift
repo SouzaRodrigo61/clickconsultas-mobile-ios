a0ff23690acf8f63453ff4f3bdbbc79d82f976e6
Rodrigo Santos de Souza <souza.rodrigo61@gmail.com>
2025-08-17 18:02:57 -0300

    feat: Adiciona dependências remotas e arquivo de resolução de pacotes para o projeto ClickConsultas Mobile iOS, incluindo bibliotecas essenciais como Firebase, Google SignIn e SwiftUI Introspect.

--- Project.swift
+++ Project.swift
@@ -2,6 +2,15 @@ import ProjectDescription
 
 let project = Project(
     name: "clickconsultas-mobile-ios",
+    packages: [
+        .remote(url: "https://github.com/pointfreeco/swift-composable-architecture", requirement: .exact("1.20.1")),
+        .remote(url: "https://github.com/1998code/SwiftGlass.git", requirement: .exact("1.8.0")),
+        .remote(url: "https://github.com/Aeastr/Portal.git", requirement: .exact("2.0.0")),
+        .remote(url: "https://github.com/nathantannar4/Transmission", requirement: .exact("2.5.3")),
+        .remote(url: "https://github.com/firebase/firebase-ios-sdk.git", requirement: .exact("12.1.0")),
+        .remote(url: "https://github.com/google/GoogleSignIn-iOS", requirement: .exact("9.0.0")),
+        .remote(url: "https://github.com/siteline/SwiftUI-Introspect", requirement: .exact("1.4.0-beta.4"))
+    ],
     targets: [
         .target(
             name: "clickconsultas-mobile-ios",
@@ -18,7 +27,17 @@ let project = Project(
             ),
             sources: ["App/Sources/**"],
             resources: ["App/Resources/**"],
-            dependencies: [],
+            dependencies: [
+                .package(product: "ComposableArchitecture"),
+                .package(product: "SwiftGlass"),
+                .package(product: "Portal"),
+                .package(product: "Transmission"),
+                .package(product: "FirebaseCrashlytics"),
+                .package(product: "FirebaseAnalytics"),
+                .package(product: "FirebaseAuth"),
+                .package(product: "GoogleSignIn"),
+                .package(product: "SwiftUIIntrospect")
+            ],
             settings: .settings(
                 base: [:],
                 configurations: [
